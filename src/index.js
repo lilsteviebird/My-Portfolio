e5bd60fe1a96e2f7993f3477ecf239579dfba1e2
lilsteviebird <stevenli880@gmail.com>
2020-10-13 00:24:55 -0400

    created router and succesful rrouting components

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+//imported stuff for react mdl
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+
+//import stuff for browser router
+import { BrowserRouter } from 'react-router-dom';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
