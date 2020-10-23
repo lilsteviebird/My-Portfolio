a3294ab960d3708810c73f4eca781803070f2dae
lilsteviebird <stevenli880@gmail.com>
2020-10-23 00:08:00 -0400

    added links

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,12 +15,14 @@ class Projects extends Component {
         <div className="projects-grid">
           {/* Project 1 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #1</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Snake Game Project</CardTitle>
             <CardText>
               React project code for a simple game of snake to play online! Simple animation and a reactive UI to view your score. 
             </CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
+              <a href="https://github.com/lilsteviebird/Simple-Snake-Game" rel = "noopener noferrer" target="_blank">
+                <Button colored>GitHub</Button>
+              </a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -29,12 +31,14 @@ class Projects extends Component {
 
           {/* Project 2 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Portfolio Project</CardTitle>
             <CardText>
               React project code for a simple website that displys my portfolio! Check it out and feel free to share!
             </CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
+              <a href="https://github.com/lilsteviebird/My-Portfolio" rel = "noopener noferrer" target="_blank">
+                <Button colored>GitHub</Button>
+              </a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
@@ -43,12 +47,14 @@ class Projects extends Component {
 
           {/* Project 3 */}
           <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Native Project #1</CardTitle>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >Calorie Tracker App</CardTitle>
             <CardText>
               React Native code to help Deerfield Academy students keep track of their calories from meals that are provided for by the school!
             </CardText>
             <CardActions border>
-              <Button colored>GitHub</Button>
+              <a href="https://github.com/lilsteviebird/DAEatz" rel = "noopener noferrer" target="_blank">
+                <Button colored>GitHub</Button>
+              </a>
             </CardActions>
             <CardMenu style={{color: '#fff'}}>
               <IconButton name="share" />
